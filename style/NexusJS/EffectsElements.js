59918ea1d50fb7893328838592dc18d170574074
andrewbertax96 <andres.bertazzi@gmail.com>
2022-08-09 17:33:20 +0200

    sliders box effect

--- style/NexusJS/EffectsElements.js
+++ style/NexusJS/EffectsElements.js
@@ -23,3 +23,39 @@ let reverb_onOff = new Nexus.Toggle('#reverb-onOff',{
    'size': [40,20],
    'state': false
 });
+
+let chorusLFO = new Nexus.Dial('#chorusLFO',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let chorusDelayTime = new Nexus.Dial('#chorusDelayTime',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let chorusDepths = new Nexus.Dial('#chorusDepths',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let number = new Nexus.Number('#number',{
+  'size': [30,30],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+number.link(chorusLFO);
