ed79c8546b96390972433141e8c1933596f4f6ba
andrewbertax96 <andres.bertazzi@gmail.com>
2022-08-11 01:57:48 +0200

    Pedal Parameters

--- style/NexusJS/EffectsElements.js
+++ style/NexusJS/EffectsElements.js
@@ -1,25 +1,25 @@
 //Pedal Box elements
-let chorus_onOff = new Nexus.Toggle('#chorus-onOff',{
+let chorus_onOff = new Nexus.Toggle('#chorus_onOff',{
    'size': [40,20],
    'state': false
 });
-let vibrato_onOff = new Nexus.Toggle('#vibrato-onOff',{
+let vibrato_onOff = new Nexus.Toggle('#vibrato_onOff',{
    'size': [40,20],
    'state': false
 });
-let tremolo_onOff = new Nexus.Toggle('#tremolo-onOff',{
+let tremolo_onOff = new Nexus.Toggle('#tremolo_onOff',{
    'size': [40,20],
    'state': false
 });
-let phaser_onOff = new Nexus.Toggle('#phaser-onOff',{
+let phaser_onOff = new Nexus.Toggle('#phaser_onOff',{
    'size': [40,20],
    'state': false
 });
-let delay_onOff = new Nexus.Toggle('#delay-onOff',{
+let delay_onOff = new Nexus.Toggle('#delay_onOff',{
    'size': [40,20],
    'state': false
 });
-let reverb_onOff = new Nexus.Toggle('#reverb-onOff',{
+let reverb_onOff = new Nexus.Toggle('#reverb_onOff',{
    'size': [40,20],
    'state': false
 });
@@ -51,6 +51,97 @@ let chorusDepths = new Nexus.Dial('#chorusDepths',{
    'step': 0,
    //'value': filterCh1[0].frequency.value
 });
+let vibratoFreq = new Nexus.Dial('#vibratoFreq',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let vibratoDepths = new Nexus.Dial('#vibratoDepths',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let tremoloFreq = new Nexus.Dial('#tremoloFreq',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let tremoloDepths = new Nexus.Dial('#tremoloDepths',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let phaserFreq = new Nexus.Dial('#phaserFreq',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let phaserOctave = new Nexus.Dial('#phaserOctave',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let phaserBaseFreq = new Nexus.Dial('#phaserBaseFreq',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let delayTune = new Nexus.Dial('#delayTune',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let reverbDecay = new Nexus.Dial('#reverbDecay',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+let reverbSeconds = new Nexus.Dial('#reverbSeconds',{
+   'size': [30,30],
+   'interaction': 'vertical', // "radial", "vertical", or "horizontal"
+   'mode': 'relative', // "absolute" or "relative"
+   'min': 20,
+   'max': 5000,
+   'step': 0,
+   //'value': filterCh1[0].frequency.value
+});
+
 
 let chorusLFO_number = new Nexus.Number('#chorusLFO_number',{
   'size': [45,20],
@@ -73,6 +164,87 @@ let chorusDepths_number = new Nexus.Number('#chorusDepths_number',{
   'max': 5000,
   'step': 1
 });
+let vibratoFreq_number = new Nexus.Number('#vibratoFreq_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let vibratoDepths_number = new Nexus.Number('#vibratoDepths_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let tremoloFreq_number = new Nexus.Number('#tremoloFreq_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let tremoloDepths_number = new Nexus.Number('#tremoloDepths_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let phaserFreq_number = new Nexus.Number('#phaserFreq_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let phaserOctave_number = new Nexus.Number('#phaserOctave_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let phaserBaseFreq_number = new Nexus.Number('#phaserBaseFreq_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let delayTune_number = new Nexus.Number('#delayTune_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let reverbDecay_number = new Nexus.Number('#reverbDecay_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+let reverbSeconds_number = new Nexus.Number('#reverbSeconds_number',{
+  'size': [45,20],
+  'value': 0,
+  'min': 20,
+  'max': 5000,
+  'step': 1
+});
+
 chorusLFO_number.link(chorusLFO);
 chorusDelayTime_number.link(chorusDelayTime);
 chorusDepths_number.link(chorusDepths);
+vibratoFreq_number.link(vibratoFreq);
+vibratoDepths_number.link(vibratoDepths);
+tremoloFreq_number.link(tremoloFreq);
+tremoloDepths_number.link(tremoloDepths);
+phaserFreq_number.link(phaserFreq);
+phaserOctave_number.link(phaserOctave);
+phaserBaseFreq_number.link(phaserBaseFreq);
+delayTune_number.link(delayTune);
+reverbDecay_number.link(reverbDecay);
+reverbSeconds_number.link(reverbSeconds);
