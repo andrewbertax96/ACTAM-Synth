88bec08e0c184f00af7c79f49bae8d064f088a6e
andrewbertax96 <andres.bertazzi@gmail.com>
2022-08-11 03:02:24 +0200

    remove content line class

--- style/NexusJS/SynthElements.js
+++ style/NexusJS/SynthElements.js
@@ -6,12 +6,10 @@ let osc1_shape = new Nexus.Select('#osc1-shape',{
    'size': [100,30],
    'options': ['sine','triangle','square','sawtooth']
 });
-
 let osc1_onOff = new Nexus.Toggle('#osc1-onOff',{
    'size': [40,20],
    'state': false
 });
-
 let osc1_det = new Nexus.Number('#osc1-det',{
    'size': [45,30],
    'min': -200,
@@ -19,17 +17,14 @@ let osc1_det = new Nexus.Number('#osc1-det',{
    'step': 1,
    'value': 0
 });
-
 let fir1_toggle = new Nexus.Toggle('#fir1-toggle',{
    'size': [40,20],
    'state': false
 });
-
 let fir1_type = new Nexus.Select('#fir1-type',{
    'size': [100,30],
    'options': ['lowpass','highpass','bandpass']
 });
-
 let fir1_cut = new Nexus.Dial('#fir1-cut',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
@@ -39,7 +34,6 @@ let fir1_cut = new Nexus.Dial('#fir1-cut',{
    'step': 0,
    'value': filterCh1[0].frequency.value
 });
-
 let osc1_vol = new Nexus.Dial('#osc1-vol',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
@@ -49,17 +43,14 @@ let osc1_vol = new Nexus.Dial('#osc1-vol',{
    'step': 0,
    'value': volume1.gain.value
 });
-
 let osc2_shape = new Nexus.Select('#osc2-shape',{
    'size': [100,30],
    'options': ['sine','triangle','square','sawtooth']
 });
-
 let osc2_onOff = new Nexus.Toggle('#osc2-onOff',{
    'size': [40,20],
    'state': false
 });
-
 let osc2_det = new Nexus.Number('#osc2-det',{
    'size': [45,30],
    'min': -200,
@@ -67,17 +58,14 @@ let osc2_det = new Nexus.Number('#osc2-det',{
    'step': 1,
    'value': 0
 });
-
 let fir2_toggle = new Nexus.Toggle('#fir2-toggle',{
    'size': [40,20],
    'state': false
 });
-
 let fir2_type = new Nexus.Select('#fir2-type',{
    'size': [100,30],
    'options': ['lowpass','highpass','bandpass']
 });
-
 let fir2_cut = new Nexus.Dial('#fir2-cut',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
@@ -87,14 +75,12 @@ let fir2_cut = new Nexus.Dial('#fir2-cut',{
    'step': 0,
    'value': filterCh2[0].frequency.value
 });
-
 let piano = new Nexus.Piano('#keyboard',{
    'size': [710,150],
    'mode': 'button',  // 'button', 'toggle', or 'impulse'
    'lowNote': 36,
    'highNote': 108
 });
-
 let osc2_vol = new Nexus.Dial('#osc2-vol',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
@@ -104,7 +90,6 @@ let osc2_vol = new Nexus.Dial('#osc2-vol',{
    'step': 0,
    'value': volume2.gain.value
 });
-
 let osc_att = new Nexus.Dial('#osc-att',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
@@ -114,7 +99,6 @@ let osc_att = new Nexus.Dial('#osc-att',{
    'step': 0,
    'value': 0.5
 });
-
 let osc_dec = new Nexus.Dial('#osc-dec',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
@@ -124,7 +108,6 @@ let osc_dec = new Nexus.Dial('#osc-dec',{
    'step': 0,
    'value': 0.5
 });
-
 let osc_sus = new Nexus.Dial('#osc-sus',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
@@ -134,7 +117,6 @@ let osc_sus = new Nexus.Dial('#osc-sus',{
    'step': 0,
    'value': 0.5
 });
-
 let osc_rel = new Nexus.Dial('#osc-rel',{
    'size': [30,30],
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
