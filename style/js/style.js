1a6312eb2dbe9daff5532fba772c51bf3aaf3b4a
andrewbertax96 <andres.bertazzi@gmail.com>
2022-08-29 12:37:49 +0200

    remove style.js

--- style/js/style.js
+++
@@ -1,8 +0,0 @@
-function loadScript(fileJS){
-  const script = document.createElement("script");
-  script.src = fileJS;
-  document.body.append(script);
-}
-loadScript("NexusJS/EffectsElements.js");
-loadScript("NexusJS/SynthElements.js");
-loadScript("NexusJS/Keyboard.js");
