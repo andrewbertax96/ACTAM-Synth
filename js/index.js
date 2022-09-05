ab5c51f259abeb8eb31b117e6e02a612ee2afca0
Olga <olga.besedova@gmail.com>
2022-09-05 21:40:05 +0400

    webmidi added

--- js/index.js
+++ js/index.js
@@ -35,6 +35,54 @@ navigator.requestMIDIAccess().then((midiAccess) =>
    }
  }
 
+ WebMidi.enable(function () {
+
+  // Viewing available inputs and outputs
+  console.log(WebMidi.inputs);
+  console.log(WebMidi.outputs);
+
+  // Retrieve an input by name, id or index
+  let input = WebMidi.inputs[0];
+
+  // OR...
+  // input = WebMidi.getInputById("1809568182");
+  // input = WebMidi.inputs[0];
+
+  // Listen for a 'note on' message on all channels
+  input.addListener('noteon', 'all',
+      function (e) {
+          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").");
+      }
+  );
+
+  input.addListener('noteoff', 'all',
+  function (e) {
+      console.log("Received 'noteoff' message (" + e.note.name + e.note.octave + ").");
+  }
+);
+
+  // Listen to pitch bend message on channel 3
+  input.addListener('pitchbend', 3,
+      function (e) {
+          console.log("Received 'pitchbend' message.", e);
+      }
+  );
+
+  // Listen to control change message on all channels
+  input.addListener('controlchange', "all",
+      function (e) {
+          console.log("Received 'controlchange' message.", e);
+      }
+  );
+
+  // Remove all listeners for 'noteoff' on all channels
+  //input.removeListener('noteoff');
+
+  // Remove all listeners on the input
+  //input.removeListener();
+
+});
+
  let activeSynths = new Map();
 
  function midiNoteOn(key)
